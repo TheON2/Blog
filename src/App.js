b71220b5adeb281ccf2f0d8301c1b51b6a80a6ca
TheON2 <sniperad@naver.com>
2023-01-01 23:29:44 +0900

    new project

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import {useState} from "react";
+
+function App() {
+
+  let post = '강남 우동 맛집';
+  let [글제목1, set글제목1] = useState('남자 코트 추천');
+  let [글제목2, set글제목2] = useState('강남 우동맛집');
+  let [글제목3, set글제목3] = useState('파이썬독학');
+  let [logo , setLogo] = useState('ReactBlog');
+
+  return (
+    <div className="App">
+      <div className="black-nav">
+        <h4>{logo}</h4>
+      </div>
+      <div className="list">
+        <h4>{글제목1}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목2}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목3}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
